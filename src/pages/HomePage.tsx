3725c860848cbb6a443dff1bdce632d70537c5a6
Michael Chop <007micle007@gmail.com>
2022-11-05 12:22:28 +0300

    Added interaction with the github api

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import {useSearchUsersQuery} from "../store/github/github.api";
 
 const HomePage = () => {
+    const {isLoading, isError, data} = useSearchUsersQuery('micle')
+
+    console.log(data)
     return (
         <div>
             home
